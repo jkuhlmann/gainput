1efdbe9b8e0734e62d12625538af9ebbf282d178
Johannes Kuhlmann <git@johanneskuhlmann.de>
2013-07-08 14:27:41 +0200

    Fixed streams not being destructed properly.

--- lib/source/gainput/dev/GainputStream.h
+++ lib/source/gainput/dev/GainputStream.h
@@ -17,6 +17,8 @@ namespace gainput {
 class Stream
 {
 public:
+	virtual ~Stream() { }
+
 	virtual size_t Read(void* dest, size_t length) = 0;
 	virtual size_t Write(const void* src, size_t length) = 0;
 
